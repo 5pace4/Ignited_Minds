97b98f944544b2a2ae9dc5ba0894ad5c33374a29
RJ-Hossan <mdrefajhossan@gmail.com>
2023-06-06 22:14:25 +0600

    Signup password matching & email checking via js_see this changes

--- js/main.js
+++ js/main.js
@@ -40,6 +40,52 @@ faqs.forEach(faq => {
     // });
 
 
+        // Sign Up Verification
+        function validation(){
+            var form = document.getElementById("form");
+            var text = document.getElementById("text");
+            var email = document.getElementById("email").value;
+            var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+            if(email.match(pattern)){
+                form.classList.add("Valid");
+                form.classList.remove("Invalid");
+                text.innerHTML = "Valid Email Address";
+                text.style.color = "green";
+            }
+            else{
+                form.classList.remove("Valid");
+                form.classList.add("Invalid");
+                text.innerHTML = "*Invalid Email Address";
+                text.style.color = "red";
+            }
+            if(email == ""){
+                form.classList.remove("Valid");
+                form.classList.remove("Invalid");
+                text.innerHTML = "";
+            }
+        }
+
+        // Password Matching
+        function passfunction(){
+            var password = document.querySelector('.password').value,
+                confirm_pass = document.querySelector('.confirmpassword').value;
+            var text1 = document.getElementById("text1");
+            if(password == " "){
+                text1.innerHTML = "Password field can't be empty!!!";
+                text1.style.color = "orange";
+            }
+            else if(password != confirm_pass){
+                text1.innerHTML = "Password didn't match.";
+                text1.style.color = "green";
+                return false;
+            }
+            else if(password == confirm_pass){
+                text1.innerHTML = "Password Matched.";
+                text1.style.color = "green";
+                return true;
+            }
+        }
+
         // Open the modal
         function openModal_s() {
             document.getElementById("signupModal").style.display = "block";
